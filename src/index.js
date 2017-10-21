310e64a67ba5a9a3e8124798087ebc35852edb34
Vinícius Goulart <goulart.vs@gmail.com>
2017-10-21 18:35:40 +0200

    Sets project up

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import React from "react";
+import { render } from "react-dom";
+
+render(<div>Hello world</div>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+const vendor = ["react", "react-dom", "redux", "react-redux", "whatwg-fetch"];
+let devtool = "eval-source-map";
+const extensions = [".js", ".jsx", ".css", ".scss", ".json"];
+let plugins = [
+  new webpack.optimize.ModuleConcatenationPlugin(),
+  new webpack.DefinePlugin({
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: ["vendor", "manifest"]
+  }),
+  new HtmlWebpackPlugin({
+    template: "src/index.html",
+    chunksSortMode: "dependency"
+  }),
+  new ExtractTextPlugin({
+    filename: "style.[contenthash].css",
+    allChunks: true
+  })
+];
+
+module.exports = (env = {}) => {
+  if (env.prod) {
+    devtool = "source-map";
+  }
+
+  if (env.analyze) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return {
+    entry: {
+      bundle: "./src/index.js",
+      vendor
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "[name].[chunkhash].js",
+      publicPath: "/"
+    },
+    module: {
+      rules: [
+        {
+          use: "babel-loader",
+          test: /\.jsx?$/,
+          exclude: /node_modules/
+        },
+        {
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "postcss-loader"]
+          }),
+          test: /\.s?css$/
+        }
+      ]
+    },
+    resolve: { extensions },
+    plugins,
+    devServer: {
+      port: 8080,
+      historyApiFallback: true
+    }
+  };
+};
